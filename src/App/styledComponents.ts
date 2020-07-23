436f2ac21b4190b50aa38e70bfc86f3523d4b7a0
Srikanth <srikanthbhamidipalli.3@gmail.com>
2020-07-23 23:11:36 +0530

    added API and handled loading

---
+++ src/App/styledComponents.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  display: flex;
+  flex: 1;
+  height: 100vh;
+  justify-content: center;
+  padding-top: 30px;
+  box-sizing: border-box;
+`;
+
+export const LoadingText = styled.span`
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 1.25;
+  color: #171f46;
+  align-self: center;
+`;
